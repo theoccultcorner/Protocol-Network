4e469d96c7400237411cc0ba58144837e09e4920
Raven The wolf <theoccultcorner@gmail.com>
2023-03-30 21:14:10 -0700

    commit

---
+++ pages/api/generate.js
@@ -0,0 +1,83 @@
+import { Configuration, OpenAIApi } from "openai";
+require('dotenv').config()
+import client from "twilio";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+ 
+const twilioClient = client(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+function generatePrompt(name, phoneNumber, make, model, year, vin, problemDescription) {
+  return ` 
+
+  Act as the boss and tell Andrew that we have a job to do. instruct him to
+  follow up with the customer 
+  ${name} 
+  at ${phoneNumber}, 
+  the vehicle is a ${year} ${make} ${model}
+  with a vin number of ${vin}.
+ 
+  the customer said
+  ${problemDescription}
+  give them a call.
+  Thank you,
+  The Boss.
+  `;
+}
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      },
+    });
+    return;
+  }
+
+  const { name, make, phoneNumber, model, year, vin, problemDescription } = req.body;
+  if (!make || !model || !year || !vin || !problemDescription) {
+    res.status(400).json({
+      error: {
+        message: "Please provide make, model, year, VIN, and problem description for the vehicle",
+      },
+    });
+    return;
+  }
+
+  try {
+    const prompt = generatePrompt(name, phoneNumber, make, model, year, vin, problemDescription);
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0.6,
+      max_tokens: 1000,
+    });
+    const message = completion.data.choices[0].text;
+    const messageBody = `A Form Was Submitted\n\n${message}`;
+    // Send the message using Twilio
+    const twilioResponse = await twilioClient.messages.create({
+      body: messageBody,
+      from: '+18775224714',
+      to: '+18055541361' // Replace with the customer's phone number
+    });
+
+    res.status(200).json({ result: messageBody });
+  } catch (error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        },
+      });
+    }
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,273 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+import "materialize-css/dist/css/materialize.min.css"; 
+ 
+ 
+export default function Home() {
+  const [vehicleInput, setVehicleInput] = useState({
+    name: "",
+    phoneNumber: "",
+    make: "",
+    model: "",
+    year: "",
+    vin: "",
+    problemDescription: "",
+  });
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(vehicleInput),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setVehicleInput({
+
+        name: "",
+        phoneNumber: "",
+        make: "",
+        model: "",
+        year: "",
+        vin: "",
+        problemDescription: "",
+      });
+    } catch(error) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+
+  
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+   
+      </Head>
+ {/* Navbar */}
+ <nav>
+          <div className="nav-wrapper black">
+            <a href="#" className="brand-logo">
+              Protocol Network
+            </a>
+            <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+         
+            </a>
+            <ul className="right hide-on-med-and-down">
+              <li>
+                <a href="#about">Home</a>
+              </li>
+             
+              <li>
+                <a href="#pricing">Services Pricing</a>
+              </li>
+              
+              <li>
+                <a href="#photo">Photo Gallery</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        {/* Mobile navbar */}
+        <ul className="sidenav" id="mobile-demo">
+        <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#features">Features</a>
+              </li>
+              <li>
+                <a href="#services">Services</a>
+              </li>
+              <li>
+                <a href="#about">About Us</a>
+              </li>
+              <li>
+                <a href="#photo">Photo Gallery</a>
+              </li>
+        </ul>
+      <main className={styles.main}>
+      
+        
+
+        {/* Home section */}
+        <section className={styles.home + " container"}>
+          <h1>Protocol Network</h1>
+          <h5>
+          25+ Years of Automotive Excellence Right at Your Doorstep
+ 
+          </h5>
+          <p>
+          Colossians 
+  3:23-24
+          </p>
+        </section>
+
+     
+        <section className="section" id="pricing">
+  <div className="container">
+    <div className="row">
+      <div className="col s12 m4">
+        <div className="card">
+          <div className="card-content">
+            <span className="card-title">Basic Plan</span>
+            <h3>$50</h3>
+            <p>per service</p>
+            <ul className="pricing-feature-list">
+              <li className="pricing-feature">Oil Change</li>
+              <li className="pricing-feature">Tire Rotation</li>
+              <li className="pricing-feature">Brake Inspection</li>
+              <li className="pricing-feature">Fluid Check & Top-up</li>
+              <li className="pricing-feature">Diagnostic Check</li>
+              <li className="pricing-feature">10% Discount on Parts & Labor</li>
+            </ul>
+          </div>
+          <div className="card-action">
+            <a href="#" className="btn waves-effect waves-light black">Book Now</a>
+          </div>
+        </div>
+      </div>
+      <div className="col s12 m4">
+        <div className="card">
+          <div className="card-content">
+            <span className="card-title">Premium Plan</span>
+            <h3>$150</h3>
+            <p>per service</p>
+            <ul className="pricing-feature-list">
+              <li className="pricing-feature">All Basic Plan Services</li>
+              <li className="pricing-feature">Air Conditioning Service</li>
+              <li className="pricing-feature">Battery Replacement</li>
+              <li className="pricing-feature">Alternator Replacement</li>
+              <li className="pricing-feature">Starter Replacement</li>
+              <li className="pricing-feature">15% Discount on Parts & Labor</li>
+            </ul>
+          </div>
+          <div className="card-action">
+            <a href="#" className="btn waves-effect waves-light black">Book Now</a>
+          </div>
+        </div>
+      </div>
+      <div className="col s12 m4">
+        <div className="card">
+          <div className="card-content">
+            <span className="card-title">Ultimate Plan</span>
+            <h3>$250</h3>
+            <p>per service</p>
+            <ul className="pricing-feature-list">
+              <li className="pricing-feature">All Premium Plan Services</li>
+              <li className="pricing-feature">Transmission Service</li>
+              <li className="pricing-feature">Engine Tune-up</li>
+              <li className="pricing-feature">Fuel Injection Service</li>
+              <li className="pricing-feature">25% Discount on Parts & Labor</li>
+            </ul>
+          </div>
+          <div className="card-action">
+            <a href="#" className="btn waves-effect waves-light black">Book Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  
+  
+ 
+<section className="z-depth-3 card" id="photo">
+ 
+</section >
+      <div classNameName={styles.result} id="contact">{result}</div>
+        <form onSubmit={onSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter a name"
+              value={vehicleInput.name}
+              onChange={(e) => setVehicleInput({ ...vehicleInput, name: e.target.value })}
+            />
+          </label>
+          <label>
+           Phone Number:
+           <input
+           type="text"
+           name="phoneNumber"
+           placeholder="Enter a phone number"
+           value={vehicleInput.phoneNumber}
+           onChange={(e) => setVehicleInput({ ...vehicleInput, phoneNumber: e.target.value })}
+          />
+         </label>
+          <label>
+            Make:
+            <input
+              type="text"
+              name="make"
+              placeholder="Enter a make"
+              value={vehicleInput.make}
+              onChange={(e) => setVehicleInput({ ...vehicleInput, make: e.target.value })}
+            />
+          </label>
+          <label>
+            Model:
+            <input
+              type="text"
+              name="model"
+              placeholder="Enter a model"
+              value={vehicleInput.model}
+              onChange={(e) => setVehicleInput({ ...vehicleInput, model: e.target.value })}
+            />
+          </label>
+          <label>
+            Year:
+            <input
+              type="text"
+              name="year"
+              placeholder="Enter a year"
+              value={vehicleInput.year}
+              onChange={(e) => setVehicleInput({ ...vehicleInput, year: e.target.value })}
+            />
+          </label>
+          <label>
+            VIN:
+            <input
+              type="text"
+              name="vin"
+              placeholder="Enter a VIN"
+              value={vehicleInput.vin}
+              onChange={(e) => setVehicleInput({ ...vehicleInput, vin: e.target.value })}
+            />
+          </label>
+          <label>
+            Problem description:
+            <input
+              type="text"
+              name="problemDescription"
+              placeholder="Enter a problem description"
+              value={vehicleInput.problemDescription}
+              onChange={(e) => setVehicleInput({ ...vehicleInput, problemDescription: e.target.value })}
+            />
+          </label>
+          <input type="submit" value="Submit Form" />
+        </form>
+       
+      </main>
+    
+    </div>
+  );
+}
